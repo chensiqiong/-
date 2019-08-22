7242c4759427506b16d39fa51981248153bf0d9e
chensiqiong <1042299422@qq.com>
2019-08-22 23:48:57 +0800

    Update kits.js

--- code/kits.js
+++ code/kits.js
@@ -48,7 +48,7 @@ kits.randomHexColor = function () {
   let arrNum = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
   let color = '#';
   for (let i = 0; i < 6; i++) {
-    color += arrNum[kits.randomInt(0, 15)];;
+    color += arrNum[kits.randomInt(0, 15)];
   }
   return color;
 }
